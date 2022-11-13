44a5487458bd47795b024110b1a09fc6656f230b
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-13 10:59:17 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import Directory from "./components/directory/Directory";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Navbar from "./routes/navigation/Navbar";
+import Shop from "./routes/shop/Shop";
+import SignIn from "./routes/sign-in/SignIn";
 
 const App = () => {
   return (
-    <div className="App">
-      <Directory />
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path='sign-in' element={<SignIn />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwfNk3BgNyQSO7qcKabc1sxitrzkvz2Ag",
+  authDomain: "crwn-clothing-db-a93c6.firebaseapp.com",
+  projectId: "crwn-clothing-db-a93c6",
+  storageBucket: "crwn-clothing-db-a93c6.appspot.com",
+  messagingSenderId: "178455425459",
+  appId: "1:178455425459:web:c2428b0f26d691d73cdbc6",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
