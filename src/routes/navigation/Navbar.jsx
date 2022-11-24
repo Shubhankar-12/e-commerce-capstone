8295c69695c942a8cf37a3ab5112df49e1ea2838
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-24 21:36:05 +0530

    styled component on cart icon

--- src/routes/navigation/Navbar.jsx
+++ src/routes/navigation/Navbar.jsx
@@ -15,24 +15,31 @@ import {
 
 const Navbar = () => {
   const { currentUser } = useContext(UserContext);
+  const { setIsCartOpen } = useContext(CartContext);
+
   const signOutHandler = async () => {
     await signOutUser();
+    setIsCartOpen(false);
   };
   const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <NavigationContainer>
-        <LogoContainer to='/'>
+        <LogoContainer to='/' onClick={() => setIsCartOpen(false)}>
           <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
-          <NavLink to='/shop'>SHOP</NavLink>
+          <NavLink to='/shop' onClick={() => setIsCartOpen(false)}>
+            SHOP
+          </NavLink>
           {currentUser ? (
             <NavLink as='span' onClick={signOutHandler}>
               SIGN OUT
             </NavLink>
           ) : (
-            <NavLink to='/auth'>SIGN IN</NavLink>
+            <NavLink to='/auth' onClick={() => setIsCartOpen(false)}>
+              SIGN IN
+            </NavLink>
           )}
           <CartIcon />
         </NavLinks>
