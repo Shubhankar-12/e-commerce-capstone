0b906fef17943a57580ea5e7a20d04accb314810
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-27 22:24:12 +0530

    some ui change in auth route

--- src/routes/authentication/Authentication.jsx
+++ src/routes/authentication/Authentication.jsx
@@ -1,13 +1,20 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 
 import SignUp from "../../components/sign-up/SignUp.jsx";
 import SignIn from "../../components/sign-in/SignIn.jsx";
 import "./Authentication.scss";
 import { UserContext } from "../../contexts/user.context.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Authentication = () => {
+  const navigate = useNavigate();
+  const redirect = () => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  };
   const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  if (currentUser) redirect();
   return (
     <div className='auth-container'>
       {currentUser !== null ? (
