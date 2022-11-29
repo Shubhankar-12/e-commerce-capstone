5324fd59436d0441291de36990b409acf2ad1ae3
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-29 19:50:59 +0530

    user action on redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,6 @@
-import { compose, applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-
-// const middleware = [logger];
-
-// const composeEnhancer = compose(applyMiddleware(...middleware));
 
 export const store = configureStore({
   reducer: rootReducer,
