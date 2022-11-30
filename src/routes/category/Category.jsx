f67ea6f1b41800c1dfb5ead6575bf444036a73b4
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-30 22:43:08 +0530

    categories redux used in all file

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/ProductCard";
