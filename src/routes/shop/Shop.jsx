9f62237ce4ee27cf646e6746a8bd1cda3ca8af43
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-11-30 22:38:57 +0530

    categories redux done

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { setCategoriesMap } from "../../store/categories/category.action";
+import { getCollectionandDocuments } from "../../utils/firebase/firebase";
 import CategoriesPreview from "../categories-preview/CategoriesPreview";
 import Category from "../category/Category";
 import "./shop.styles.scss";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getCategoryMap = async () => {
+      const categoryMap = await getCollectionandDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoryMap();
+  }, []);
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
