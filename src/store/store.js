02339e4622cc00a2334359c656358a8171032a30
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-02 22:23:27 +0530

    custom middleware /

--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,17 @@ import { rootReducer } from "./root-reducer";
 
 // custom middleware
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) return next(action);
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("current State: ", store.getState());
-  next(action);
-  console.log("next State: ", store.getState());
-};
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) return next(action);
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("current State: ", store.getState());
+//   next(action);
+//   console.log("next State: ", store.getState());
+// };
 
 // middlewares used
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: [loggerMiddleware],
+  middleware: [logger],
 });
