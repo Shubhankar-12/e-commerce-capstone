0cc91df1cd9950998d2a06861239128b51f9a4e5
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-02 22:00:07 +0530

    nothing changed

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+// middlewares used
 export const store = configureStore({
   reducer: rootReducer,
   middleware: [logger],
