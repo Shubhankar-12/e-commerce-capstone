3bb39fe0bde919f635fe4a628a47531236e9be20
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-05 21:34:10 +0530

    redux-thunk replaced by saga

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,8 @@ import { rootReducer } from "./root-reducer";
 import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
+// import thunk from "redux-thunk";
 
 const persistConfig = {
   key: "root",
@@ -12,14 +13,17 @@ const persistConfig = {
   whitelist: ["cart"],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // middlewares used
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: [process.env.NODE_ENV === "development" && logger, thunk].filter(
-    Boolean
-  ),
+  middleware: [
+    process.env.NODE_ENV === "development" && logger,
+    sagaMiddleware,
+  ].filter(Boolean),
 });
 
 export const persistor = persistStore(store);
