da82ca7fb50cdc8a089336b8f8fcb0b34a072ee2
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-09 22:12:10 +0530

    styled

--- src/components/payment-form/payment.styles.js
+++ src/components/payment-form/payment.styles.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-
-import Button from "../button/button.component";
+import Button from "../button/Button";
 
 export const PaymentFormContainer = styled.div`
   height: 300px;
