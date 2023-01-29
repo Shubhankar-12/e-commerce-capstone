b8e310d49f0ac17b3e0930fd945cb0b401f3b1e5
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-29 20:43:45 +0530

    caegory saga to ts

---
+++ src/store/categories/category.saga.ts
@@ -0,0 +1,24 @@
+import { takeLatest, all, call, put } from "typed-redux-saga";
+import { getCollectionandDocuments } from "../../utils/firebase/firebase";
+import { fetchCategoryFailed, fetchCategorySuccess } from "./category.action";
+import { CATEGORIES_ACTION_TYPES } from "./category.type";
+
+export function* fetchCategoryAsync() {
+  try {
+    const categoriesArray = yield* call(getCollectionandDocuments);
+    yield* put(fetchCategorySuccess(categoriesArray));
+  } catch (error) {
+    yield* put(fetchCategoryFailed(error as Error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield* takeLatest(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORY_START,
+    fetchCategoryAsync
+  );
+}
+
+export function* categoriesSaga() {
+  yield* all([call(onFetchCategories)]);
+}
