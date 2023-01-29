948bb8aa9ad433afda20ea9bd9ba6ff287d3287e
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-29 10:48:57 +0530

    created category and categoryitem object

--- src/store/categories/category.type.ts
+++ src/store/categories/category.type.ts
@@ -3,3 +3,15 @@ export enum CATEGORIES_ACTION_TYPES {
   FETCH_CATEGORY_SUCCESS = "category/FETCH_CATEGORY_SUCCESS",
   FETCH_CATEGORY_FAILED = "category/FETCH_CATEGORY_FAILED",
 }
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+};
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
