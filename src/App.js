0af985f11bb2cf8b8842828367602811adfbade8
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-30 22:45:11 +0530

    lazy suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import Authentication from "./routes/authentication/Authentication";
+import Spinner from "./components/spinner/Spinner";
+import { checkUserSession } from "./store/user/user.acton";
 import Checkout from "./routes/checkout/Checkout";
-import Home from "./routes/home/Home";
 import Navbar from "./routes/navigation/Navbar";
 import Shop from "./routes/shop/Shop";
-import { checkUserSession } from "./store/user/user.acton";
+const Home = lazy(() => import("./routes/home/Home"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/Authentication")
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,15 +18,17 @@ const App = () => {
   }, []);
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/' element={<Navbar />}>
-          <Route index element={<Home />} />
-          {/* dynamic routing */}
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index element={<Home />} />
+            {/* dynamic routing */}
+            <Route path='shop/*' element={<Shop />} />
+            <Route path='auth' element={<Authentication />} />
+            <Route path='checkout' element={<Checkout />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
