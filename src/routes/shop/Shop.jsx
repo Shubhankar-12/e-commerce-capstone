ce7bd8d87ff39b547f0ba95fae366fed7ca7b607
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-02-01 22:33:49 +0530

    shop styles to jsx

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import { fetchCategoryStart } from "../../store/categories/category.action";
 import CategoriesPreview from "../categories-preview/CategoriesPreview";
 import Category from "../category/Category";
-import "./shop.styles.scss";
 
 const Shop = () => {
   const dispatch = useDispatch();
--- src/routes/shop/shop.styles.jsx
+++ src/routes/shop/shop.styles.jsx
@@ -1,6 +1,8 @@
-.procduct-container {
+import styled from "styled-components";
+
+export const procductContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 10px;
   row-gap: 50px;
-}
+`;
