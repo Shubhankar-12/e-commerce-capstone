e3442d5600fb3a62f1d41fa36138463ae36aef79
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-02-02 16:14:46 +0530

    no

--- src/routes/checkout/Checkout.jsx
+++ src/routes/checkout/Checkout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/chechout-item/CheckoutItem";
 import PaymentForm from "../../components/payment-form/PaymentForm";
