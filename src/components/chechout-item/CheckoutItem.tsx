89a8e0426de8f3c2651e126ac57558c1b6d1c2f9
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-02-02 17:01:14 +0530

    ts changes

--- src/components/chechout-item/CheckoutItem.tsx
+++ src/components/chechout-item/CheckoutItem.tsx
@@ -6,45 +6,56 @@ import {
   removeItemFromCard,
 } from "../../store/cart/cart.action";
 import { selectCartItems } from "../../store/cart/cart.selector";
-import "./checkout-item.styles.scss";
-const CheckoutItem = memo(({ cartItem }) => {
+import { CartItem } from "../../store/cart/cart.type";
+import {
+  Arrow,
+  BaseSpan,
+  CheckoutItemContainer,
+  ImageContainer,
+  Quantity,
+  RemoveButton,
+} from "./checkout-item.styles";
+import "./checkout-item.styles.tsx";
+
+type CheckoutItemProps = {
+  cartItem: CartItem;
+};
+
+const CheckoutItem = memo(({ cartItem }: CheckoutItemProps) => {
   const { name, quantity, price, imageUrl } = cartItem;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
 
   return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className='name'> {name} </span>
-      <span className='quantity'>
-        <div
-          className='arrow'
+      </ImageContainer>
+      <BaseSpan> {name} </BaseSpan>
+      <Quantity>
+        <Arrow
           onClick={() => {
             dispatch(removeItemFromCard(cartItems, cartItem));
           }}
         >
           &#10094;
-        </div>
+        </Arrow>
         <span className='value'>{quantity}</span>
-        <div
-          className='arrow'
+        <Arrow
           onClick={() => {
             dispatch(addItemsToCart(cartItems, cartItem));
           }}
         >
           &#10095;
-        </div>
-      </span>
-      <span className='price'> {price} </span>
-      <div
-        className='remove-button'
+        </Arrow>
+      </Quantity>
+      <BaseSpan> {price} </BaseSpan>
+      <RemoveButton
         onClick={() => dispatch(clearItemFromCart(cartItems, cartItem))}
       >
         &#10005;
-      </div>
-    </div>
+      </RemoveButton>
+    </CheckoutItemContainer>
   );
 });
 
