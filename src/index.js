c8f40cb48a2043aa7a419dc428c1f1eb5cd10cd7
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-02-05 12:25:14 +0530

    pwa done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { persistor, store } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,4 +25,5 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 reportWebVitals();
